607a961a6a6dd303955166e389efa576acb0c742
Adam <acd21tx@gmail.com>
2017-02-25 13:11:02 -0600

    initial commit of prog0_acd21.cpp

---
+++ cmake-build-debug/CMakeFiles/prog0_acd21.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for prog0_acd21.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+ * prog0_acd21.cpp
+ *
+ *  Created on: Sep 4, 2013
+ *      Author: Adam C.De Leon
+ *      Net ID: acd21
+ *      Program 0
+ *      Menu for Adam C. De Leon's Java Extraordinaire
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Adam C. De Leon's Java Extraordinaire\n";
+    cout << "\n";
+    cout << "House Roast:\n";
+    cout << "8 oz.\t$1.25\n";
+    cout << "12 oz.\t$1.75\n";
+    cout << "24 oz.\t$2.75\n";
+    cout << "\n";
+    cout << "Latte or Cappuccino:\n";
+    cout << "8 oz.\t$2.15\n";
+    cout << "12 oz.\t$3.25\n";
+    cout << "24 oz.\t$4.25\n";
+    cout << "\n";
+    cout << "Fellow GIS majors are eligible for an 8% discount!\n";
+
+    return 0;
+}
+
+
+
+
+
